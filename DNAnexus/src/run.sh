e1d804ef12296e1d0c577b4a519fc58b45ca8a3b
Mike Lin <mlin@mlin.net>
2013-03-21 01:34:52 -0700

    ran dx-build-applet

---
+++ DNAnexus/src/run.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# This script simply launches your C++ executable and contains 3 entry
+# points.
+
+main() {
+        /PhyloCSF
+}
+
+process() {
+        /PhyloCSF process
+}
+
+postprocess() {
+        /PhyloCSF postprocess
+}
